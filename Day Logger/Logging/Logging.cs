4295744d0f647fe4870ce4a10b173c48babf0c81
Weylyn-Cadwell <nicholas.skinner95@gmail.com>
2016-05-06 13:16:18 -0500

    Added StampLogger and LogService

--- Day Logger/Logging/Logging.cs
+++ Day Logger/Logging/Logging.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Day_Logger.Logging
 {
-    interface ILogger
+    /// <summary>
+    /// Provides an interface for logging information to a file.
+    /// </summary>
+    public interface ILogger
     {
-        
+        void Write(string info, LogPriority priority);
+        void Open();
+        void Close();
     }
+
+    public class StampLogger : ILogger
+    {
+        public StampLogger(string path)
+        {
+            FilePath = path;
+        }
+
+        public void Write(string info, LogPriority priority)
+        {
+
+        }
+
+        private string FilePath;
+    }
+
+    public class LogService
+    {
+        public LogService(ILogger log)
+        {
+
+        }
+    }
+
+    public enum LogPriority { NORMAL, WARNING, FATAL }
 }
