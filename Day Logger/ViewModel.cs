d0f255211cbec651e79f9c2dd412f762d6556a6a
Weylyn-Cadwell <nicholas.skinner95@gmail.com>
2016-05-13 10:31:08 -0500

    Added ViewModel for the program

---
+++ Day Logger/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Day_Logger.TimeStamps;
+using System.Collections.ObjectModel;
+
+namespace Day_Logger
+{
+    /// <summary>
+    /// The ViewModel to be used for UI interaction.
+    /// </summary>
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.Stamps = new ObservableCollection<TimeStamp>();
+        }
+
+        public ObservableCollection<TimeStamp> Stamps { get; set; }
+    }
+}
